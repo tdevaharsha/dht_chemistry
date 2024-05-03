2de401a73066bac9b0831e9cece7588764e073d1
tdevaharsha <167551732+tdevaharsha@users.noreply.github.com>
2024-05-03 22:21:36 +0530

    Add files via upload

---
+++ DHT_Chemistry.py
@@ -0,0 +1,51 @@
+NCERT Class 11
+
+Chapter 2: Structure of atom
+
+Important points
+
+-Atomic number(Z):Number of protons in an atom
+                 :Number of neutrons in an atom
+
+-Mass number: Number of protons+Number of neutrons
+
+-Dalton's Atomic Theory:
+*Atoms are indivisible and indestructible.
+*Atoms of the same element are identical in all aspects.
+*Atoms of different elements have different masses and properties.
+*Compounds are formed by the combination of atoms of different elements in small whole numbers.
+
+-Thomson's Model of Atom:
+*Discovered electrons through cathode ray tube experiments.
+*Proposed the "plum pudding" model where electrons are embedded in a positively charged sphere.
+
+-Rutherford's Model of Atom:
+*Discovered the nucleus through the gold foil experiment.
+*Proposed that the atom consists mostly of empty space with a small, dense, positively charged nucleus.
+
+-Bohr's Model of Atom:
+*Electrons revolve around the nucleus in fixed circular orbits or shells.
+*Each orbit corresponds to a specific energy level or energy state.
+*lectrons can only exist in these discrete energy levels and cannot exist between them.
+*Electrons can absorb or emit energy as they move between energy levels.
+
+-Electronic Configuration:
+*It describes the arrangement of electrons in various orbitals around the nucleus.
+*Electrons occupy orbitals in order of increasing energy levels.
+
+-Aufbau Principle:
+*Electrons fill orbitals starting from the lowest energy level to higher energy levels.
+
+-Pauli’s Exclusion Principle:
+*No two electrons in an atom can have the same set of four quantum numbers.
+
+-Hund’s Rule:
+*Electrons fill orbitals of the same energy level singly before pairing up.
+
+Important formuals:
+
+1) Bohr's radius: 0.529*n^2/Z A
+
+2)En= -13.6*Z^2/n^2 eV/atom
+
+3) E=hv
